dda247c2820f66a4212c243c92c63c2a4c4eacc9
GormHjorth <gorm.hermansen2@gmail.com>
2024-09-28 18:13:28 +0200

    Update merge.py

--- src/merge.py
+++ src/merge.py
@@ -1,30 +1,29 @@
 """Code for merging two sorted lists."""
 
-
 def merge(x:list[int], y:list[int]) -> list[int]:
     x = sorted(x)  #Sorting both lists prior to running
     y = sorted(y)
     z = []
-    
-    if not x:
-        return y
-    if not y:
-        return x
+
+    # if not x:
+    #     return y
+    # if not y:
+    #     return x
     i = 0
     j = 0
 
-    while i < len(x) or j < len(y):   #Keeps going until both lists have reached the last value
+    while i < len(x) and j < len(y):   #Keeps going until both lists have reached the last value
+    
         if x[i] < y[j]:  # If the value of the placement in the x-list is smaller than the current placement in the y-list, then we append the current x-list value
             z.append(x[i]) 
             i += 1    #And count up 1 for the x-list related value, i, so we now check the next value
         elif x[i] >= y[j]:  #If the value of x[i] is bigger (or equal to) y[j], then we append y[j]
             z.append(y[j])
             j += 1  #And count up 1 for the y-list related value
-    
+
     if i >= len(x):
-        z.append(y[j:len(y)])
+        z.extend(y[j:len(y)])
     elif j >= len(y):
-        z.append(x[i:len(x)])
-        
-        
+        z.extend(x[i:len(x)])
+    
     return(z)
